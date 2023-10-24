3d84b5f60f64c1b5258578baec00ddd4715b2811
RafGeorges <raf.georges@gmail.com>
2023-10-24 16:05:11 +0200

    content aboutpage

--- src/components/PageAbout.js
+++ src/components/PageAbout.js
@@ -13,16 +13,25 @@ export default function PageAbout(){
             </div>
             <div className="about--collapse--container">
                 <Collapse
-                    title='salut'
+                    title="Fiabilité"
+                    content='Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+                    régulièrement vérifiées par nos équipes.'
                     />
                 <Collapse
-                    title='salut'
+                    title="Respect"
+                    content='La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+                    perturbation du voisinage entraînera une exclusion de notre plateforme.'
                     />
                 <Collapse
-                title='salut'
+                title="Service"
+                content='"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. Nhésitez pas à nous contacter si vous avez la moindre question."'
                 />
                 <Collapse
-                    title='salut'
+                    title="Sécurité"
+                    content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+                    correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+                    locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
+                    également des ateliers sur la sécurité domestique pour nos hôtes."
                 /> 
             </div>
         </div>
