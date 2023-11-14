2a6df4972760dc81643a8cd73b8b4417ab03d1bf
RafGeorges <raf.georges@gmail.com>
2023-11-14 08:15:09 +0100

    final

--- src/components/PageAbout.js
+++ src/components/PageAbout.js
@@ -9,7 +9,6 @@ export default function PageAbout(){
         <>
             <Header/>
             <div className="pageAbout">
-            
             <div className="about--main">
                     <Banner
                         image={aboutBanner}
